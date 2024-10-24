9ced80d7ff5c31f776f146b209317d55b3ec504e
julie-bui <julie.bui.2@city.ac.uk>
2024-10-24 16:28:58 +0100

    Add files via upload

---
+++ HW 4/task1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE petPet (
+    	petname VARCHAR(20) PRIMARY KEY,
+    	owner VARCHAR(45),
+    	species VARCHAR(45),
+    	gender ENUM ('M', 'F'),
+    	birth DATE,
+    	death DATE
+);
+
+
+CREATE TABLE petEvent (
+    	petname VARCHAR(20),
+    	eventdate DATE,
+	eventtype VARCHAR(15),
+	remark VARCHAR(255),
+	PRIMARY KEY (petname, eventdate),
+	FOREIGN KEY (petname) REFERENCES petPet(petname)
+);
